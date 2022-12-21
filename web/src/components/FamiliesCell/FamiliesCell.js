cff56b289e273885f66c54dfc696e1d5cd66ebe0
dietler <andy@dietler.net>
2022-12-21 14:46:27 -0800

    family in tailwind on homepage

--- web/src/components/FamiliesCell/FamiliesCell.js
+++ web/src/components/FamiliesCell/FamiliesCell.js
@@ -3,6 +3,7 @@ export const QUERY = gql`
     families {
       id
       lastName
+      address
     }
   }
 `
@@ -17,14 +18,23 @@ export const Failure = ({ error }) => (
 
 export const Success = ({ families }) => {
   return (
-    <>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
       {families.map((family) => (
-        <div key={family.id}>
-          <header>
-            <h2>{family.lastName}</h2>
-          </header>
+        <div
+          key={family.id}
+          className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+        >
+          <div className="min-w-0 flex-1">
+            <a href="#" className="focus:outline-none">
+              <span className="absolute inset-0" aria-hidden="true" />
+              <p className="text-sm font-medium text-gray-900">
+                {family.lastName}
+              </p>
+              <p className="truncate text-sm text-gray-500">{family.address}</p>
+            </a>
+          </div>
         </div>
       ))}
-    </>
+    </div>
   )
 }
